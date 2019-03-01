6faa5809a57876d63f4cff2243d93d19f97e7bae
Nicholas Geary <ncgeary@buffalo.edu>
2019-03-01 13:59:08 -0500

    .

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,22 @@
 import React, { Component } from 'react';
+import AppNavbar from './components/AppNavbar';
+import ShoppingList from './components/ShoppingList';
+import ItemModal from './components/ItemModal';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Container } from 'reactstrap';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <h1>Hello</h1>
-      </div>
+      < div className = "App" >
+        <AppNavbar />
+          <Container>
+            <ItemModal />
+            <ShoppingList />
+          </Container> 
+        </div>
     );
   }
 }
